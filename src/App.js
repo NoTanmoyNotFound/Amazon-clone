6d78a2891dbaaf207decb2343e1faf8b71c5c2d0
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-04-12 13:09:19 +0530

    making header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
-    
+
     <div className="App">
-      <h1>Hello World</h1>
+      {/* <h1>Hello World</h1> */}
+      <Header />
     </div>
   );
 }
