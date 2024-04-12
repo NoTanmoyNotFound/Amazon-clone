898d8516dc5386dff913a940854f68a6e03394b0
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-04-12 11:44:09 +0530

    setting up firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyByxKN1P0Epa0dWju_lyH5ak4KixxDfY9s",
+    authDomain: "clone-d5bec.firebaseapp.com",
+    projectId: "clone-d5bec",
+    storageBucket: "clone-d5bec.appspot.com",
+    messagingSenderId: "1005660956325",
+    appId: "1:1005660956325:web:3dda3e8e9c91ab229fcf4c",
+    measurementId: "G-BP20NSP2KH"
+  };
