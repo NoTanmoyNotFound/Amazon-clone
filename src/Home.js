b39d2619bc9c1eb2fefb9c54ff47f0a0be637811
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-04-15 11:20:12 +0530

    adding products

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,19 @@ function Home() {
         <div className='home'>
             <div className="home__container">
                 <img className='home__image' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/ATFGW/MHS_GW_Candle-holders_PC-under._CB560634205_.jpg"  />
+
+                <div className="home__row">
+                    {/* product */}
+                    {/* product */}
+                </div>
+                <div className="home__row">
+                    {/* product */}
+                    {/* product */}
+                    {/* product */}
+                </div>
+                <div className="home__row">
+                    {/* product */}
+                </div>
             </div>
         </div>
     )
---
+++ src/Product.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Product.css"
+
+function Product() {
+  return (
+    <div className='product'>
+      <div className="product__info">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Product
