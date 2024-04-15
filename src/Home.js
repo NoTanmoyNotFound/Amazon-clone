ffffd912d6f55344cb5f5e656032c032e3ff9438
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-04-15 11:55:56 +0530

    launching products

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Home.css"
+import Product from './Product'
 
 function Home() {
     return (
@@ -8,7 +9,9 @@ function Home() {
                 <img className='home__image' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/ATFGW/MHS_GW_Candle-holders_PC-under._CB560634205_.jpg"  />
 
                 <div className="home__row">
-                    {/* product */}
+                    <Product />
+                    <Product />
+                    
                     {/* product */}
                 </div>
                 <div className="home__row">
--- src/Product.js
+++ src/Product.js
@@ -2,13 +2,23 @@ import React from 'react'
 import "./Product.css"
 
 function Product() {
-  return (
-    <div className='product'>
-      <div className="product__info">
-        
-      </div>
-    </div>
-  )
+    return (
+        <div className='product'>
+            <div className="product__info">
+                <p>Steve Jobs by Walter Isaacson [Hardcover]</p>
+                <p className="product__price">
+                    <small>₹</small>
+                    <strong>499</strong>
+                </p>
+                <div className="proudct__rating">
+                    <p>⭐</p>
+                    
+                </div>
+                <img src="https://m.media-amazon.com/images/I/51iau+M4IBL._AC_UY218_.jpg" alt="" />
+                <button>Add to Cart</button>
+            </div>
+        </div>
+    )
 }
 
 export default Product
