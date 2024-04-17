11c46a6e326ba99f3aa79b7b07595cc4b8ae62cc
NoTanmoyNotFound <tanmoydas78416@gmail.com>
2024-04-17 13:11:09 +0530

    adding porperly cards

--- src/Home.js
+++ src/Home.js
@@ -6,21 +6,33 @@ function Home() {
     return (
         <div className='home'>
             <div className="home__container">
-                <img className='home__image' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/ATFGW/MHS_GW_Candle-holders_PC-under._CB560634205_.jpg"  />
+                <img className='home__image' src="https://images-eu.ssl-images-amazon.com/images/G/31/IMG20/Home/2024/Gateway/ATFGW/MHS_GW_Candle-holders_PC-under._CB560634205_.jpg" />
 
                 <div className="home__row">
-                    <Product />
-                    <Product />
-                    
-                    {/* product */}
+                    <Product
+                        id ="12345678"
+                        title={"Steve Jobs by Walter Isaacson[Hardcover]"}
+                        price={499}
+                        image={"https://m.media-amazon.com/images/I/51iau+M4IBL._AC_UY218_.jpg"}
+                        rating={4}
+                    />
+                    <Product
+                        id ="22345678"
+                        title={"Steve Jobs by Walter Isaacson[Hardcover]"}
+                        price={499}
+                        image={"https://m.media-amazon.com/images/I/51iau+M4IBL._AC_UY218_.jpg"}
+                        rating={4} 
+                    />
+
+
                 </div>
                 <div className="home__row">
-                    {/* product */}
-                    {/* product */}
-                    {/* product */}
+                    <Product />
+                    <Product />
+                    <Product />
                 </div>
                 <div className="home__row">
-                    {/* product */}
+                    <Product />
                 </div>
             </div>
         </div>
--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import "./Product.css"
 
-function Product() {
+function Product({ title, image, price, rating }) {
     return (
         <div className='product'>
             <div className="product__info">
-                <p>Steve Jobs by Walter Isaacson [Hardcover]</p>
+                <p>{title}</p>
                 <p className="product__price">
                     <small>₹</small>
-                    <strong>499</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className="proudct__rating">
-                    <p>⭐</p>
-                    
+                    {Array(rating).fill().map((_, i) => (
+                        <p>⭐</p>
+                    ))}
+
                 </div>
-                <img src="https://m.media-amazon.com/images/I/51iau+M4IBL._AC_UY218_.jpg" alt="" />
-                <button>Add to Cart</button>
+
             </div>
+            <img src={image} alt="" />
+            <button>Add to Cart</button>
         </div>
     )
 }
